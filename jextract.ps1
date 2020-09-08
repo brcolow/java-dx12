95a109c920a0e170612ea4966d4c4bf5310e763f
Michael Ennen <mike.ennen@gmail.com>
2020-09-07 23:30:06 -0700

    Mess around trying to get jextract to work with d3d12.

--- jextract.ps1
+++ jextract.ps1
@@ -1,4 +1,5 @@
-# http://hg.openjdk.java.net/panama/dev/raw-file/841483f2887f/doc/panama_foreign.html#windows-notes
+# http://hg.openjdk.java.net/panama/dev/raw-file/foreign/doc/panama_foreign.html#hello-world-c-header-helloworld.h
 $inc ="C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0"
+$lib = "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17763.0"
 $panamaHome = "C:\Program Files\Java\jdk-14-panama"
-& $panamaHome\bin\jextract -J-Xmx8G -L C:\Windows\System32\ -I $inc\um -l d3d12 -o d3d12.jar --record-library-path $inc\um\d3d12.h
+& $panamaHome\bin\jextract -J-Xmx8G --log FINEST -L "C:\Windows\System32" -L "$lib\um\x64" -I "$inc\um" -l d3d12 -o d3d12.jar --record-library-path $inc\um\d3d12.h
