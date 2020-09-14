d723b4ad5c02998bf30d294fbb655357fced8994
Michael Ennen <mike.ennen@gmail.com>
2020-09-14 12:19:16 -0700

    Huge thanks to Jorn Vernee, Maurizio Cimadamore, and Ty Young!

--- src/main/java/com/dx12/DX12.java
+++ src/main/java/com/dx12/DX12.java
@@ -2,7 +2,6 @@ package com.dx12;
 
 import com.dx12.d3d12_h;
 import com.dx12.dxgi_h;
-import com.dx12.dxgi_h$constants$0;
 import jdk.incubator.foreign.FunctionDescriptor;
 import jdk.incubator.foreign.MemoryAddress;
 import jdk.incubator.foreign.MemoryLayout;
@@ -10,22 +9,26 @@ import jdk.incubator.foreign.MemorySegment;
 import jdk.incubator.foreign.LibraryLookup;
 import jdk.incubator.foreign.NativeScope;
 import jdk.incubator.foreign.MemoryAccess;
-import jdk.incubator.foreign.MemoryHandles;
 import jdk.incubator.foreign.SequenceLayout;
+
 import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.invoke.VarHandle;
 import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.IntStream;
 
-import static com.dx12.dxgi_h.IDXGIFactory1Vtbl.EnumAdapters1$VH;
-import static com.dx12.dxgi_h.IDXGIFactory1Vtbl.EnumAdapters1$get;
-import static com.dx12.dxgi_h.IDXGIFactory1Vtbl.EnumAdapters1$set;
+import static com.dx12.dxgi_h.DXGI_ADAPTER_DESC1;
+import static com.dx12.dxgi_h.IDXGIAdapter1;
+import static com.dx12.dxgi_h.IDXGIAdapter1Vtbl;
+import static com.dx12.dxgi_h.IDXGIFactory1;
+import static com.dx12.dxgi_h.IDXGIFactory1Vtbl;
 import static jdk.incubator.foreign.CSupport.*;
 import static jdk.incubator.foreign.MemoryLayout.PathElement.groupElement;
+
 /**
-* https://cr.openjdk.java.net/~mcimadamore/panama/ffi.html#appendix-full-source-code
-*/
+ * https://cr.openjdk.java.net/~mcimadamore/panama/ffi.html#appendix-full-source-code
+ */
 public class DX12 {
 
     public enum IID {
@@ -39,11 +42,11 @@ public class DX12 {
 
     private static final Map<IID, MemorySegment> GUID_MAP = Map.of(
             IID.IID_IDXGIFactory1,
-            //   0x770aae78, 0xf26f, 0x4dba,           0xa8, 0x29, 0x25, 0x3c, 0x83, 0xd1, 0xb3, 0x87
-            GUID(0x770aae78, 0xf26f, 0x4dba, new int[]{0xa8, 0x29, 0x25, 0x3c, 0x83, 0xd1, 0xb3, 0x87}),
+            //   0x770aae78, 0xf26f, 0x4dba,            0xa8, 0x29, 0x25, 0x3c, 0x83, 0xd1, 0xb3, 0x87
+            GUID(0x770aae78, 0xf26f, 0x4dba, new int[] {0xa8, 0x29, 0x25, 0x3c, 0x83, 0xd1, 0xb3, 0x87}),
             IID.IID_IDXGIAdapter1,
-            //   0x29038f61, 0x3839, 0x4626,           0x91, 0xfd, 0x08, 0x68, 0x79, 0x01, 0x1a, 0x05
-            GUID(0x29038f61, 0x3839, 0x4626, new int[]{0x91, 0xfd, 0x08, 0x68, 0x79, 0x01, 0x1a, 0x05}));
+            //   0x29038f61, 0x3839, 0x4626,            0x91, 0xfd, 0x08, 0x68, 0x79, 0x01, 0x1a, 0x05
+            GUID(0x29038f61, 0x3839, 0x4626, new int[] {0x91, 0xfd, 0x08, 0x68, 0x79, 0x01, 0x1a, 0x05}));
 
     public static MemorySegment GUID(int data1, int data2, int data3, int[] data4) {
         MemoryLayout GUID = MemoryLayout.ofStruct(
@@ -62,75 +65,71 @@ public class DX12 {
         data1Handle.set(segment, data1);
         data2Handle.set(segment, (short) data2);
         data3Handle.set(segment, (short) data3);
-        for (int i = 0; i < 8; i++) {
-            data4Handle.set(segment, i, (byte) data4[i]);
-        }
+        IntStream.range(0, 8).forEachOrdered(i -> data4Handle.set(segment, i, (byte) data4[i]));
         return segment;
     }
 
-    // https://docs.microsoft.com/en-us/windows/win32/direct3d12/creating-a-basic-direct3d-12-component
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         LibraryLookup d3d12 = LibraryLookup.ofLibrary("D3D12");
         LibraryLookup dxgi = LibraryLookup.ofLibrary("dxgi");
-        try (var scope = NativeScope.unboundedScope()) {
+        try (NativeScope scope = NativeScope.unboundedScope()) {
             // IDXGIFactory1** dxgiFactory;
-            var dxgiFactory = scope.allocate(C_POINTER);
-            // https://docs.microsoft.com/en-us/windows/win32/api/dxgi/nf-dxgi-createdxgifactory1
-            // HRESULT = CreateDXGIFactory1(_uuidof(dxgiFactory), &dxgiFactory))
-            int hresult = dxgi_h.CreateDXGIFactory1(GUID(IID.IID_IDXGIFactory1), dxgiFactory);
-            System.out.println("hresult: " + hresult);
-            var dxgiAdapter = scope.allocate(C_POINTER);
-            MemoryAddress address = MemorySegment.allocateNative(dxgi_h.IDXGIFactory1Vtbl.$LAYOUT().select(MemoryLayout.PathElement.groupElement("EnumAdapters1"))).address();
-            FunctionDescriptor functionDescriptor = FunctionDescriptor.of(C_INT, // Return type = HRESULT
-                    dxgi_h.IDXGIFactory1.$LAYOUT(), // arg0 = IDXGIFactory1 * This
-                    C_INT.withName("Adapter"), // arg1 = UINT Adapter
-                    C_POINTER // arg1 = IDXGIAdapter1 **ppAdapter
-            );
-            MethodType methodType = MethodType.methodType(int.class, MemorySegment.class, int.class, MemoryAddress.class);
-            MethodHandle methodHandle = getSystemLinker().downcallHandle(address, methodType, functionDescriptor);
-            try {
-                hresult = (int) methodHandle.invokeExact(dxgiFactory, 1, dxgiAdapter.address());
-            } catch (Throwable ex) {
-                ex.printStackTrace();
-            }
-            System.out.println("vh: " + EnumAdapters1$VH());
-            //EnumAdapters1$get(dxgiFactory, MemoryAccess.getAddress(dxgiAdapter));
-            // Now we need to implement and call the following:
-            /*
-            void GetHardwareAdapter(IDXGIFactory4* pFactory, IDXGIAdapter1** ppAdapter)
-            {
-                *ppAdapter = nullptr;
-                for (UINT adapterIndex = 0; ; ++adapterIndex)
-                {
-                    IDXGIAdapter1* pAdapter = nullptr;
-                    if (DXGI_ERROR_NOT_FOUND == dxgiFactory->EnumAdapters1(adapterIndex, &pAdapter))
-                    {
-                        // No more adapters to enumerate.
-                        break;
-                    }
+            var ppDxgiFactory = IDXGIFactory1.allocatePointer(scope);
+            // HRESULT = CreateDXGIFactory1(_uuid(dxgiFactory), &dxgiFactory))
+            checkResult(dxgi_h.CreateDXGIFactory1(GUID(IID.IID_IDXGIFactory1), ppDxgiFactory));
+            // IDXGIFactory1*
+            MemorySegment pDxgiFactory = asSegment(MemoryAccess.getAddress(ppDxgiFactory), IDXGIFactory1.$LAYOUT());
 
-                    // Check to see if the adapter supports Direct3D 12, but don't create the
-                    // actual device yet.
-                    if (SUCCEEDED(D3D12CreateDevice(pAdapter, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr)))
-                    {
-                        *ppAdapter = pAdapter;
-                        return;
-                    }
-                    pAdapter->Release();
-                }
-            }
-             */
-            //for (int adapterIndex = 0; ; adapterIndex++) {
-                // WRONG: dxgiFactory.EnumAdapters1(adapterIndex, dxgiAdapter);
-                // dxgiFactory->EnumAdapters1(adapterIndex, &dxgiAdapter)
-            //}
-            //getSystemLinker().downcallHandle(dxgi_h.IDXGIFactory1Vtbl.EnumAdapters1$VH()
-            // ComPtr<IDXGIAdapter1> hardwareAdapter;
-            // define_guid!(IID_IDXGIAdapter1,0x29038f61, 0x3839, 0x4626, 0x91, 0xfd, 0x08, 0x68, 0x79, 0x01, 0x1a, 0x05);
-            // GetHardwareAdapter(factory.Get(), &hardwareAdapter);
-            // D3D12CreateDevice(hardwareAdapter.Get(),D3D_FEATURE_LEVEL_11_0,IID_PPV_ARGS(&m_device))
-            // https://docs.microsoft.com/en-us/windows/win32/api/guiddef/ns-guiddef-guid
-            // https://github.com/bryal/dxgi-rs/blob/d319030ad095cda42e3d46ac3eb9ebddd6e73b48/src/constants.rs#L77
+            // (This)->lpVtbl
+            MemorySegment vtbl = asSegment(IDXGIFactory1.lpVtbl$get(pDxgiFactory), IDXGIFactory1Vtbl.$LAYOUT());
+            // lpVtbl->EnumAdapters1
+            MemoryAddress addrEnumAdapters = IDXGIFactory1Vtbl.EnumAdapters1$get(vtbl);
+
+            // link the pointer
+            MethodHandle MH_EnumAdapters1 = getSystemLinker().downcallHandle(
+                    addrEnumAdapters,
+                    MethodType.methodType(int.class, MemoryAddress.class, int.class, MemoryAddress.class),
+                    FunctionDescriptor.of(C_INT, C_POINTER, C_INT, C_POINTER));
+
+            /* [annotation][out] _COM_Outptr_  IDXGIAdapter1** */
+            MemorySegment ppOut = IDXGIAdapter1.allocatePointer(scope);
+            checkResult((int) MH_EnumAdapters1.invokeExact(pDxgiFactory.address(), 0, ppOut.address()));
+            // IDXGIAdapter1*
+            MemorySegment pAdapter = asSegment(MemoryAccess.getAddress(ppOut), IDXGIAdapter1.$LAYOUT());
+
+            // (This)->lpVtbl
+            MemorySegment vtbl2 = asSegment(IDXGIAdapter1.lpVtbl$get(pAdapter), IDXGIAdapter1Vtbl.$LAYOUT());
+            // lpVtbl->EnumAdapters1
+            // HRESULT(*)(IDXGIAdapter1*,DXGI_ADAPTER_DESC1*)
+            MemoryAddress addrGetDesc1 = IDXGIAdapter1Vtbl.GetDesc1$get(vtbl2);
+
+            // link the pointer
+            MethodHandle MH_GetDesc1 = getSystemLinker().downcallHandle(
+                    addrGetDesc1,
+                    MethodType.methodType(int.class, MemoryAddress.class, MemoryAddress.class),
+                    FunctionDescriptor.of(C_INT, C_POINTER, C_POINTER));
+
+            /* DXGI_ADAPTER_DESC1* */
+            MemorySegment pDesc = DXGI_ADAPTER_DESC1.allocate(scope);
+            checkResult((int) MH_GetDesc1.invokeExact(pAdapter.address(), pDesc.address()));
+
+            // print description
+            MemorySegment descStr = DXGI_ADAPTER_DESC1.Description$slice(pDesc);
+            String str = new String(descStr.toByteArray(), StandardCharsets.UTF_16LE);
+            System.out.println(str);
+        }
+    }
+
+    public static MemorySegment asSegment(MemoryAddress addr, MemoryLayout layout) {
+        return MemorySegment.ofNativeRestricted(addr, layout.byteSize(), Thread.currentThread(), null, null);
+    }
+
+    private static final int S_OK = 0x00000000;
+
+    private static void checkResult(int result) {
+        switch (result) {
+            case S_OK -> {}
+            default -> throw new IllegalStateException("Unknown result: " + String.format("%X8", result));
         }
     }
 }
