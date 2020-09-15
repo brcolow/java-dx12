5992e47fb05d25669be6c47a0169c346ba2d218d
Michael Ennen <mike.ennen@gmail.com>
2020-09-15 11:29:02 -0700

    Start fleshing out win32 Window creation.

--- jextract.ps1
+++ jextract.ps1
@@ -1,6 +1,10 @@
 $jdk = "C:\Users\mikee\dev\panama-foreign\build\windows-x86_64-server-release\jdk"
 nal -Name jextract -Value "$jdk\bin\jextract.exe"
 $I = "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0"
+# This does nothing because Windows.h is purely includes and we are using --filter, but according to Wikipedia:
+# Many of these files cannot simply be included by themselves (they are not self-contained), because of dependencies.
+# So this will need to be trial and error on what we need. We definitely need the CreateWindowEx function.
+jextract --source -d . -t com.dx12 -I "$I\um" -C "-D WIN32_LEAN_AND_MEAN" --filter "windows.h" -- "$I\um\Windows.h"
 jextract --source -d . -t com.dx12 -I "$I\um" --filter "d3d12.h" -- "$I\um\d3d12.h"
 jextract --source -d . -t com.dx12 -I "$I\um" --filter "d3dcommon.h" -- "$I\um\d3dcommon.h"
 jextract --source -d . -t com.dx12 -I "$I\um" --filter "dxgi.h" -- "$I\shared\dxgi.h"
--- src/main/java/com/dx12/DX12.java
+++ src/main/java/com/dx12/DX12.java
@@ -69,6 +69,36 @@ public class DX12 {
         }
     }
 
+    public static void createWindow() {
+        /*
+            // Initialize the window class.
+            WNDCLASSEX windowClass = { 0 };
+            windowClass.cbSize = sizeof(WNDCLASSEX);
+            windowClass.style = CS_HREDRAW | CS_VREDRAW;
+            windowClass.lpfnWndProc = WindowProc;
+            windowClass.hInstance = hInstance;
+            windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+            windowClass.lpszClassName = L"DXSampleClass";
+            RegisterClassEx(&windowClass);
+
+            RECT windowRect = { 0, 0, static_cast<LONG>(pSample->GetWidth()), static_cast<LONG>(pSample->GetHeight()) };
+            AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+            // Create the window and store a handle to it.
+            m_hwnd = CreateWindow(
+                windowClass.lpszClassName,
+                pSample->GetTitle(),
+                WS_OVERLAPPEDWINDOW,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                windowRect.right - windowRect.left,
+                windowRect.bottom - windowRect.top,
+                nullptr,        // We have no parent window.
+                nullptr,        // We aren't using menus.
+                hInstance,
+                pSample);
+         */
+    }
     public static void main(String[] args) throws Throwable {
         LibraryLookup d3d12 = LibraryLookup.ofLibrary("D3D12");
         LibraryLookup dxgi = LibraryLookup.ofLibrary("dxgi");
